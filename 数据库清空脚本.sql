3b2bc62712babe6b7e608e250d8f96cc09320f14
qishuai <qishuai@qq.com>
2020-12-25 17:56:21 +0800

    first commit

---
+++ 数据库清空脚本.sql
@@ -0,0 +1,93 @@
+truncate TABLE singlewindows.gen_table;
+truncate TABLE singlewindows.gen_table_column;
+truncate TABLE singlewindows.gen_table_copy;
+truncate TABLE singlewindows.goodsdeclare_deccontainer;
+truncate TABLE singlewindows.goodsdeclare_deccoplimit;
+truncate TABLE singlewindows.goodsdeclare_decfreetxt;
+truncate TABLE singlewindows.goodsdeclare_decgoodslimit;
+truncate TABLE singlewindows.goodsdeclare_decgoodslimitvin;
+truncate TABLE singlewindows.goodsdeclare_dechead;
+truncate TABLE singlewindows.goodsdeclare_declicensedocus;
+truncate TABLE singlewindows.goodsdeclare_declist;
+truncate TABLE singlewindows.goodsdeclare_decmarklob;
+truncate TABLE singlewindows.goodsdeclare_decotherpack;
+truncate TABLE singlewindows.goodsdeclare_decrequestcert;
+truncate TABLE singlewindows.goodsdeclare_decsign;
+truncate TABLE singlewindows.goodsdeclare_decsupplement;
+truncate TABLE singlewindows.goodsdeclare_decuser;
+truncate TABLE singlewindows.goodsdeclare_edocrealation;
+truncate TABLE singlewindows.goodsdeclare_sddtax;
+truncate TABLE singlewindows.goodsdeclare_trncontagoodslist;
+truncate TABLE singlewindows.goodsdeclare_trncontainer;
+truncate TABLE singlewindows.goodsdeclare_trnhead;
+truncate TABLE singlewindows.goodsdeclare_trnlist;
+truncate TABLE singlewindows.i_animals_body;
+truncate TABLE singlewindows.i_animals_head;
+truncate TABLE singlewindows.i_waybill_body;
+truncate TABLE singlewindows.i_waybill_head;
+
+// ------------------------------
+truncate TABLE singlewindows.manifest_additionalinformation;
+truncate TABLE singlewindows.manifest_address;
+truncate TABLE singlewindows.manifest_agent;
+truncate TABLE singlewindows.manifest_bordertransportmeans;
+truncate TABLE singlewindows.manifest_carrier;
+truncate TABLE singlewindows.manifest_classification;
+truncate TABLE singlewindows.manifest_commodity;
+truncate TABLE singlewindows.manifest_communication;
+truncate TABLE singlewindows.manifest_consignee;
+truncate TABLE singlewindows.manifest_consignment;
+truncate TABLE singlewindows.manifest_consignmentbordertransportmeans;
+truncate TABLE singlewindows.manifest_consignmentbordertransportmeans_itinerary;
+truncate TABLE singlewindows.manifest_consignmentitem;
+truncate TABLE singlewindows.manifest_consignor;
+truncate TABLE singlewindows.manifest_consolidator;
+truncate TABLE singlewindows.manifest_contact;
+truncate TABLE singlewindows.manifest_control;
+truncate TABLE singlewindows.manifest_declaration;
+truncate TABLE singlewindows.manifest_deconsolidator;
+truncate TABLE singlewindows.manifest_freight;
+truncate TABLE singlewindows.manifest_goodsmeasure;
+truncate TABLE singlewindows.manifest_governmentagencygoodsitem;
+truncate TABLE singlewindows.manifest_governmentprocedure;
+truncate TABLE singlewindows.manifest_head;
+truncate TABLE singlewindows.manifest_loadinglocation;
+truncate TABLE singlewindows.manifest_master;
+truncate TABLE singlewindows.manifest_notifyparty;
+truncate TABLE singlewindows.manifest_packaging;
+truncate TABLE singlewindows.manifest_representativeperson;
+truncate TABLE singlewindows.manifest_submitter;
+truncate TABLE singlewindows.manifest_transitdeparture;
+truncate TABLE singlewindows.manifest_transitdestination;
+truncate TABLE singlewindows.manifest_transportcontractdocument;
+truncate TABLE singlewindows.manifest_transportcontractdocument_amendment;
+truncate TABLE singlewindows.manifest_transportequipment;
+truncate TABLE singlewindows.manifest_undgcontact;
+truncate TABLE singlewindows.manifest_unloadinglocation;
+
+
+
+
+
+
+//--------------------------------------
+truncate TABLE singlewindows.tax_bussinessdata;
+truncate TABLE singlewindows.tax_commonresponsemessage;
+truncate TABLE singlewindows.tax_datainfo;
+truncate TABLE singlewindows.tax_envelopinfo;
+truncate TABLE singlewindows.tax_invtmessage;
+truncate TABLE singlewindows.tax_keyinfo;
+truncate TABLE singlewindows.tax_nemsacmprlmessage;
+truncate TABLE singlewindows.tax_nemsacmprltype;
+truncate TABLE singlewindows.tax_nemsinvtcbecbilltype;
+truncate TABLE singlewindows.tax_nemsinvtdecheadtype;
+truncate TABLE singlewindows.tax_nemsinvtdeclisttype;
+truncate TABLE singlewindows.tax_nemsinvtgoodstype;
+truncate TABLE singlewindows.tax_nemsinvtheadtype;
+truncate TABLE singlewindows.tax_nemsinvtlisttype;
+truncate TABLE singlewindows.tax_nemsinvtwarehousetype;
+truncate TABLE singlewindows.tax_operinfo;
+truncate TABLE singlewindows.tax_package;
+truncate TABLE singlewindows.tax_signature;
+truncate TABLE singlewindows.tax_signedinfo;
+truncate TABLE singlewindows.tax_signedinfo_reference;
